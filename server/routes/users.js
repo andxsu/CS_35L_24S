65586dd8fd8c7cb896eec255cd073493f4435ca6
Aryan Mosavian Pour <aryanmp16@gmail.com>
2024-05-24 21:39:41 -0700

    Added Users.js

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,2 +1,27 @@
-const router = require('express').Router();
-let User = require('../')
+import express from "express";
+import db from "../db/connection.js";
+import { ObjectId } from "mongodb";
+const router = express.Router();
+
+//let User = require('../models/users.model');
+//import User from "../models/users.model.js"
+
+//Route that actually handles incoming http "get" requests
+//if the end of our url is just 'users/' and we have a get request:
+router.get("/", async (req, res) => {
+    let collection = db.collection("users"); //retrieves the "users" collection from the database.
+    let results = await collection.find({}).toArray();
+    res.send(results).status(200);
+});
+
+//handles incoming http "post" requests
+// router.route('/add').post((req, res) => {
+//     const username = req.body.username;
+//     const newUser = new User({username});
+//     newUser.save()
+//         .then(() => res.json('User added!'))
+//         .catch(err => res.status(400).json('Error: ' + err));
+// });
+
+//module.exports = router;
+export default router;
--- server/server.js
+++ server/server.js
@@ -1,13 +1,17 @@
 import express from "express";
 import cors from "cors";
 import records from "./routes/record.js";
+import usersRouter from "./routes/users.js";
 
 const PORT = /*process.env.PORT ||*/ 5000;
 const app = express();
 
+//const usersRouter = require('./routes/users.js');
+
 app.use(cors());
 app.use(express.json());
 app.use("/record", records);
+app.use("/users", usersRouter);
 
 // start the Express server
 app.listen(PORT, () => {

959c64017d5d6beb544d5b3ce307eeb4006588bd
Aryan Mosavian Pour <aryanmp16@gmail.com>
2024-05-24 23:24:33 -0700

    Added creation function to Users

--- server/routes/users.js
+++ server/routes/users.js
@@ -6,22 +6,44 @@ const router = express.Router();
 //let User = require('../models/users.model');
 //import User from "../models/users.model.js"
 
+let collection = db.collection("users"); //retrieves the "users" collection from the database.
+
 //Route that actually handles incoming http "get" requests
 //if the end of our url is just 'users/' and we have a get request:
-router.get("/", (req, res) => {
-    let collection = db.collection("users"); //retrieves the "users" collection from the database.
-    let results = collection.find({});//.toArray();
-    res.send(results).status(200);
+router.get("/", async (req, res) => {
+    try {
+        let results = await collection.find({}).toArray();
+        res.status(200).send(results);
+    }
+    catch (error) {
+        res.status(500).send(error.message);
+    }
 });
 
 //handles incoming http "post" requests
-// router.route('/add').post((req, res) => {
-//     const username = req.body.username;
-//     const newUser = new User({username});
-//     newUser.save()
-//         .then(() => res.json('User added!'))
-//         .catch(err => res.status(400).json('Error: ' + err));
-// });
+router.post("/add", async (req, res) =>{
+    const username = req.body.username;
+    const address = req.body.address;
+    const email = req.body.email;
+    const phoneNum = Number(req.body.phoneNum);
 
-//module.exports = router;
+    const newEntry = {
+        username,
+        address,
+        email,
+        phoneNum
+    };
+
+    try {
+        const result = await collection.insertOne(newEntry);
+        if (result.acknowledged)
+            res.status(200).send(result);
+        else
+            res.status(500).send("Failed to add entry");
+    }
+    catch (error) {
+        res.status(500).send("Error adding entry: " + error.message);
+    }
+});
+
 export default router;

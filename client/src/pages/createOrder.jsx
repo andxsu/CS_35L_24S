fd19b81601bf5d560cd9e2a56637aa1b57bbcd04
Andrew Su <andrewsu485@gmail.com>
2024-06-02 19:58:53 -0700

    added order functionality to main branch

--- client/src/pages/createOrder.jsx
+++ client/src/pages/createOrder.jsx
@@ -1,71 +1,342 @@
+import { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { BuildYourOwnBurritoBowl } from "./BuildYourOwn";
+import { BuildYourOwnRendeWestSalad } from "./BuildYourOwn";
+import { BuildYourOwnBurrito } from "./BuildYourOwn";
+import { BuildYourOwnTacos } from "./BuildYourOwn";
+import { BuildYourOwnTacoSalad } from "./BuildYourOwn";
+import { BuildYourOwnPizza } from "./BuildYourOwn";
+import { BuildYourOwnSandwich } from "./BuildYourOwn";
+import { BuildYourOwnStudySalad } from "./BuildYourOwn";
+import { BuildYourOwnBreakfastSkillet } from "./BuildYourOwn";
+import { BuildYourOwnBagel } from "./BuildYourOwn";
 import {useContext} from 'react';
-import {UserContext} from '../../context/userContext';
-import React from 'react';
-import {useState, useEffect} from 'react';
-import axios from 'axios';
 import {toast} from 'react-hot-toast' ;
-import {useNavigate} from 'react-router-dom';
+import axios from 'axios';
+import {UserContext} from '../../context/userContext';
 
 export default function CreateOrder() {
-    const navigate = useNavigate();
-    const {user} = useContext(UserContext);
-    
-    const [data, setData] = useState({
-        dining_hall: '',
-        creator_username: '',
-        food_order: '',
-        notes_for_deliverer: '',
-        active: true,
-        out_for_delivery: false,
-    })
 
-    useEffect(() => {
-        if (!user) {
-            navigate('/login');
-        }
-        if (user){
-            setData(prevData => ({
-                ...prevData,
-                creator_username: user.username
-            }))
-        }
-    }, [user, navigate]);
-    
-    
-    const createorder = async (e) => {
-        e.preventDefault();
-        const {dining_hall, creator_username, food_order, notes_for_deliverer, active, out_for_delivery} = data;
-        try {
-            const {data} = await axios.post('/order', {dining_hall, creator_username, food_order, notes_for_deliverer, active, out_for_delivery})
-            if(data.error){
-                toast.error(data.error)
-            }
-            else{
-                setData({});
-                toast.success('Order created!');
-                navigate('/dashboard');
-            }
-        } catch (error) {
-            
-        }
+const params = useParams();
+const navigate = useNavigate();
+const {user} = useContext(UserContext);
+
+const [form, setForm] = useState({
+    dining_hall: '',
+    creator_username: '',
+    food_order: '',
+    notes_for_deliverer: '',
+    protein: '',
+    side1: '',
+    side2: '',
+    toppings1: [],
+    toppings14: '',
+    toppings2: '',
+    toppings3: '',
+    beverage: '',
+    sauce: '',
+    cheese: '',
+    topping: '',
+    addOns1: '',
+    addOns2: '',
+    addOns3: '',
+    bread: '',
+    cheese: '',
+    spreadsCondiments: [],
+    greens: '',
+    dressing: '',
+    croutons: '',
+    eggs: '',
+    bagel: '',
+    active: true,
+    out_for_delivery: false,
+})
+
+useEffect(() => {
+    if (!user) {
+        navigate('/login');
     }
-    return(
-        <div>
-        <form onSubmit={createorder}>
-            <label>Dining hall</label> 
-            <input type = 'text' placeholder = 'Enter dining hall...' value = {data.dining_hall} onChange={(e) => setData({...data, dining_hall: e.target.value})}/>
-            
-            <label>Food order</label> 
-            <input type = 'text' placeholder = 'Enter food orer...' value = {data.food_order} onChange={(e) => setData({...data, food_order: e.target.value})}/>
-           
-            <label>Notes for deliverer</label> 
-            <input type= 'text' placeholder = 'Enter notes...' value = {data.notes_for_deliverer} onChange={(e) => setData({...data, notes_for_deliverer: e.target.value})}/>
-           
-            <button type = 'submit'>Place order</button>
+    if (user){
+        setForm(prevData => ({
+            ...prevData,
+            creator_username: user.username
+        }))
+    }
+}, [user, navigate]);
+
+  const [isNew, setIsNew] = useState(true);
+  const [menuItems, setMenuItems] = useState([]);
+
+  const buildYourOwnItems = [
+    "Build your own bowl",
+    "Build your own burrito",
+    "Build your own burrito bowl",
+    "Build your own pizza",
+    "Build your own sandwich",
+    "Build your own Study salad", "Build your own bagel", "Build your own breakfast skillet", "Build your own tacos", "Build your own taco salad", "Build your own Rende West salad"
+  ];
+  const [buildYourOwnForm, setBuildYourOwnForm] = useState({});
 
 
-        </form>
-    </div>
-    )
+  
+  function updateForm(value) {
+    setForm((prev) => ({ ...prev, ...value }));
+    setBuildYourOwnForm((prev) => ({ ...prev, ...value }));
+  }
+  useEffect(() => {
+    const diningHallsMenus = {
+      "Rendezvous East": ["California Sushi Bowl", "Build your own bowl"],
+      "Rendezvous West": ["Build your own burrito", "Build your own burrito bowl", "Build your own tacos","Build your own taco salad","Build your own Rende West salad", "Chicken Quesadillas"],
+      "The Study": ["Build your own bagel", "Build your own breakfast skillet", "Build your own pizza", "Build your own sandwich", "Build your own Study salad", "Pretzel and sausage platter", "Swiss fondue frites", "Cream and fruits waffle", "Nutella Waffle", "Coffee"],
+      "The Drey": ["California Roll", "Cucumber avocado roll", "Berry Smoothie", "Roast Beef Sandwich", "BLT"],
+      "Bruin Cafe": ["BBQ Beef Brisket Sandwich", "Buffalo Sandwich", "Cheesesteak", "Chicken Caesar", "The Cuban"]
+    };
+
+    setMenuItems(diningHallsMenus[form.dining_hall] || []);
+  }, [form.dining_hall]);
+
+  const createorder = async (e) => {
+    e.preventDefault();
+    const concatenatedOrder = [
+        form.protein && `Protein: ${form.protein}`,
+        form.side1 && `Side 1: ${form.side1}`,
+        form.side2 && `Side 2: ${form.side2}`,
+        form.toppings1.length > 0 && `Toppings 1: ${form.toppings1.join(', ')}`,
+        form.toppings2 && `Toppings 2: ${form.toppings2}`,
+        form.toppings3 && `Toppings 3: ${form.toppings3}`,
+        form.beverage && `Beverage: ${form.beverage}`,
+        form.sauce && `Sauce: ${form.sauce}`,
+        form.cheese && `Cheese: ${form.cheese}`,
+        form.topping && `Topping: ${form.topping}`,
+        form.addOns1 && `Add-Ons 1: ${form.addOns1}`,
+        form.addOns2 && `Add-Ons 2: ${form.addOns2}`,
+        form.addOns3 && `Add-Ons 3: ${form.addOns3}`,
+        form.bread && `Bread: ${form.bread}`,
+        form.spreadsCondiments.length > 0 && `Spreads/Condiments: ${form.spreadsCondiments.join(', ')}`,
+        form.greens && `Greens: ${form.greens}`,
+        form.dressing && `Dressing: ${form.dressing}`,
+        form.croutons && `Croutons: ${form.croutons}`,
+        form.eggs && `Eggs: ${form.eggs}`,
+        form.bagel && `Bagel: ${form.bagel}`,
+    ].filter(Boolean).join(', ').replace(/[\r\n\s]{2,}/gm, " ");
+    
+
+    const { 
+        dining_hall, 
+        creator_username, 
+        food_order, 
+        notes_for_deliverer, 
+        active, 
+        out_for_delivery 
+    } = { 
+        dining_hall: form.dining_hall, 
+        creator_username: form.creator_username, 
+        food_order: concatenatedOrder, 
+        notes_for_deliverer: form.notes_for_deliverer, 
+        active: form.active, 
+        out_for_delivery: form.out_for_delivery 
+    };
+    
+    try {
+        const {data} = await axios.post('/order', {dining_hall, creator_username, food_order, notes_for_deliverer, active, out_for_delivery})
+        if(data.error){
+            toast.error(data.error)
+        }
+        else{
+            setForm({});
+            setBuildYourOwnForm({});
+            console.log(data);
+            toast.success('Order created!');
+            navigate('/dashboard');
+        }
+    } catch (error) {
+        
+    }
 }
+
+  function renderBuildYourOwnForm() {
+    switch (form.food_item) {
+      case "Build your own burrito bowl":
+        return <BuildYourOwnBurritoBowl form={buildYourOwnForm} updateForm={updateForm} />;
+      case "Build your own Rende West salad":
+        return <BuildYourOwnRendeWestSalad form={buildYourOwnForm} updateForm={updateForm} />;
+      case "Build your own burrito":
+        return <BuildYourOwnBurrito form={buildYourOwnForm} updateForm={updateForm} />;
+      case "Build your own tacos":
+        return <BuildYourOwnTacos form={buildYourOwnForm} updateForm={updateForm} />;
+      case "Build your own taco salad":
+        return <BuildYourOwnTacoSalad form={buildYourOwnForm} updateForm={updateForm} />;
+      case "Build your own pizza":
+        return <BuildYourOwnPizza form={buildYourOwnForm} updateForm={updateForm} />;
+      case "Build your own sandwich":
+        return <BuildYourOwnSandwich form={buildYourOwnForm} updateForm={updateForm} />;
+      case "Build your own Study salad":
+        return <BuildYourOwnStudySalad form={buildYourOwnForm} updateForm={updateForm} />;
+      case "Build your own breakfast skillet":
+        return <BuildYourOwnBreakfastSkillet form={buildYourOwnForm} updateForm={updateForm} />;
+      case "Build your own bagel":
+        return <BuildYourOwnBagel form={buildYourOwnForm} updateForm={updateForm} />;
+      default:
+        return null;
+    }
+  }
+
+  return (
+    <>
+      <h3 className="text-lg font-semibold p-4">Create/Update Order Request</h3>
+      <form onSubmit={createorder} className="border rounded-lg overflow-hidden p-4">
+        <div className="grid grid-cols-1 gap-x-8 gap-y-10 border-b border-slate-900/10 pb-12 md:grid-cols-2">
+          <div>
+            <h2 className="text-base font-semibold leading-7 text-slate-900">
+              Place Your Order
+            </h2>
+            <p className="mt-1 text-sm leading-6 text-slate-600">
+              This information will be displayed publicly so be careful what you share.
+            </p>
+          </div>
+
+          <div className="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 ">
+            <div>
+              <fieldset className="mt-4">
+                <legend className="sr-only">Food</legend>
+                <div className="space-y-4 sm:flex sm:items-center sm:space-x-10 sm:space-y-0">
+                  <div className="flex items-center">
+                    <input
+                      id="dhallRendeEast"
+                      name="diningHallOptions"
+                      type="radio"
+                      value="Rendezvous East"
+                      className="h-4 w-4 border-slate-300 text-slate-600 focus:ring-slate-600 cursor-pointer"
+                      checked={form.dining_hall === "Rendezvous East"}
+                      onChange={(e) => updateForm({ dining_hall: e.target.value })}
+                    />
+                    <label
+                      htmlFor="dhallRendeEast"
+                      className="ml-3 block text-sm font-medium leading-6 text-slate-900 mr-4"
+                    >
+                      Rende East
+                    </label>
+                    <input
+                      id="dhallRendeWest"
+                      name="diningHallOptions"
+                      type="radio"
+                      value="Rendezvous West"
+                      className="h-4 w-4 border-slate-300 text-slate-600 focus:ring-slate-600 cursor-pointer"
+                      checked={form.dining_hall === "Rendezvous West"}
+                      onChange={(e) => updateForm({ dining_hall: e.target.value })}
+                    />
+                    <label
+                      htmlFor="dhallRendeWest"
+                      className="ml-3 block text-sm font-medium leading-6 text-slate-900 mr-4"
+                    >
+                      Rende West
+                    </label>
+                    <input
+                      id="dhallStudy"
+                      name="diningHallOptions"
+                      type="radio"
+                      value="The Study"
+                      className="h-4 w-4 border-slate-300 text-slate-600 focus:ring-slate-600 cursor-pointer"
+                      checked={form.dining_hall === "The Study"}
+                      onChange={(e) => updateForm({ dining_hall: e.target.value })}
+                    />
+                    <label
+                      htmlFor="dhallStudy"
+                      className="ml-3 block text-sm font-medium leading-6 text-slate-900 mr-4"
+                    >
+                      The Study
+                    </label>
+                    <input
+                      id="dhallBcaf"
+                      name="diningHallOptions"
+                      type="radio"
+                      value="Bruin Cafe"
+                      className="h-4 w-4 border-slate-300 text-slate-600 focus:ring-slate-600 cursor-pointer"
+                      checked={form.dining_hall === "Bruin Cafe"}
+                      onChange={(e) => updateForm({ dining_hall: e.target.value })}
+                    />
+                    <label
+                      htmlFor="dhallBcaf"
+                      className="ml-3 block text-sm font-medium leading-6 text-slate-900 mr-4"
+                    >
+                      Bruin Cafe
+                    </label>
+                    <input
+                      id="dhallTheDrey"
+                      name="diningHallOptions"
+                      type="radio"
+                      value="The Drey"
+                      className="h-4 w-4 border-slate-300 text-slate-600 focus:ring-slate-600 cursor-pointer"
+                      checked={form.dining_hall === "The Drey"}
+                      onChange={(e) => updateForm({ dining_hall: e.target.value })}
+                    />
+                    <label
+                      htmlFor="dhallTheDrey"
+                      className="ml-3 block text-sm font-medium leading-6 text-slate-900 mr-4"
+                    >
+                      The Drey
+                    </label>
+                  </div>
+                </div>
+              </fieldset>
+            </div>
+            <div className="sm:col-span-4">
+              <label htmlFor="menuItem" className="block text-sm font-medium leading-6 text-slate-900">
+                Menu Item
+              </label>
+              <div className="mt-2">
+                <select
+                  id="menuItem"
+                  name="menuItem"
+                  className="block w-full mt-1 rounded-md border-slate-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                  value={form.food_item}
+                  onChange={(e) => updateForm({ food_item: e.target.value })}
+                  disabled={!form.dining_hall}
+                >
+                  <option value="">
+                    {form.food_item ? form.food_item : "Select a Menu Item"}
+                  </option>
+                  {menuItems.map((item, index) => (
+                    <option key={index} value={item}>
+                      {item}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            </div>
+            <div className="sm:col-span-4">
+            {renderBuildYourOwnForm()}
+            </div>
+            <div className="sm:col-span-4">
+              <label htmlFor="notes" className="block text-sm font-medium leading-6 text-slate-900">
+                Notes for deliverer
+              </label>
+              <div className="mt-2">
+                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-slate-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                  <input
+                    type="text"
+                    name="notes"
+                    id="notes"
+                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-slate-900 placeholder:text-slate-400 focus:ring-0 sm:text-sm sm:leading-6"
+                    placeholder="Leave it at my door..."
+                    value={form.notes_for_deliverer}
+                    onChange={(e) => updateForm({ notes_for_deliverer: e.target.value })}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <input
+          type="submit"
+          value="Save"
+          className="inline-flex items-center justify-center whitespace-nowrap text-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-slate-100 hover:text-accent-foreground h-9 rounded-md px-3 cursor-pointer mt-4"
+        />
+      </form>
+    </>
+  );
+}
+
+
+
 

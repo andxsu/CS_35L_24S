73d6b58f594e39a4fc2eb6eaf0ec4ee9325c3382
Matthew Schinsing <matthewslaptop@Matthews-MacBook-Pro.local>
2024-06-04 19:15:18 -0700

    Added routes for the favorite feature to authRoutes.js

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,15 +1,33 @@
 const express = require('express');
 const router = express.Router();
 const cors = require('cors');
-const {test, registerUser, loginUser, getProfile, logoutUser, forgotPassword, updateUser, deleteUser} = require('../controllers/authController');
-const {createOrder, getOrder, getAllOrders, acceptOrder, completeOrder} = require('../controllers/orderController');
+const {
+    test,
+    registerUser,
+    loginUser,
+    getProfile,
+    logoutUser,
+    forgotPassword,
+    updateUser,
+    deleteUser
+} = require('../controllers/authController');
+const {
+    createOrder,
+    getOrder,
+    getAllOrders,
+    acceptOrder,
+    completeOrder,
+    toggleFavorite,
+    getFavoriteOrders
+} = require('../controllers/orderController');
+
 //middleware
 router.use(
     cors({
         credentials: true,
         origin: 'http://localhost:5173'
     })
-)
+);
 
 //user auth routes
 router.get('/', test);
@@ -17,16 +35,20 @@ router.get('/profile', getProfile);
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.post('/logout', logoutUser);
-router.post('/forgot', forgotPassword)
+router.post('/forgot', forgotPassword);
 router.patch('/update', updateUser);
 router.delete('/delete', deleteUser);
 
 //order routes
 router.post('/order', createOrder);
 router.post('/acceptorder', acceptOrder);
-router.post('/completeorder', completeOrder)
+router.post('/completeorder', completeOrder);
 router.get('/getorder', getOrder);
 router.get('/getallorders', getAllOrders);
 
+// favorite order routes
+router.post('/togglefavorite', toggleFavorite);
+router.get('/favoriteorders', getFavoriteOrders);
 
 module.exports = router;
+

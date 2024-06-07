6420ea95b8c6fff0b2ecd789a6fe96b8948f0257
Matthew Schinsing <matthewslaptop@Matthews-MacBook-Pro.local>
2024-06-07 11:57:45 -0700

    Implemented sorting filters for previous orders on the dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -9,6 +9,8 @@ export default function Dashboard() {
     const [activeOrders, setActiveOrders] = useState([]);
     const [pastOrders, setPastOrders] = useState([]);
     const { user } = useContext(UserContext);
+    const [sortBy, setSortBy] = useState("");
+    const [dropdownOpen, setDropdownOpen] = useState(false);
 
     const fetchOrderDetails = async () => {
         let orderDetails;
@@ -55,6 +57,24 @@ export default function Dashboard() {
             console.error('Error toggling favorite:', error);
         }
     };
+
+    const sortOrders = (orders, criteria) => {
+        if (criteria === "favorite") {
+            return orders.sort((a, b) => b.orderDetails.favorite - a.orderDetails.favorite);
+        } else if (criteria === "order") {
+            return orders.sort((a, b) => a.orderDetails.food_order.localeCompare(b.orderDetails.food_order));
+        } else if (criteria === "location") {
+            return orders.sort((a, b) => a.orderDetails.dining_hall.localeCompare(b.orderDetails.dining_hall));
+        } else {
+            return orders;
+        }
+    };
+
+    const handleSortChange = (criteria) => {
+        setSortBy(prevSortBy => prevSortBy === criteria ? "" : criteria);
+    };
+
+    const sortedPastOrders = sortOrders([...pastOrders], sortBy);
 
     useEffect(() => {
         fetchOrderDetails();
@@ -105,7 +125,7 @@ export default function Dashboard() {
                 <Link to='/order' style={buttonStyle} onMouseOver={(e) => e.currentTarget.style.backgroundColor = '#357ABD'}
                     onMouseOut={(e) => e.currentTarget.style.backgroundColor = '#747bff'}
                 >
-                    Place an order
+                    Place an order 📬
                 </Link>
             )}
             <h2 style={{ marginBottom: '20px', padding: '15px', fontSize: '30px', color: '#555' }}>
@@ -139,42 +159,106 @@ export default function Dashboard() {
             <h2 style={{ marginBottom: '20px', padding: '15px', fontSize: '30px', color: '#555' }}>
                 Previous orders:
             </h2>
-            {pastOrders.length > 0 ? (
-                <ul style={{ listStyleType: 'none', padding: 0 }}>
-                    {[...pastOrders].reverse().map(order => (
-                        <li key={order.orderId} style={{
-                            border: '1px solid #ccc',
-                            borderRadius: '10px',
-                            marginBottom: '10px',
-                            padding: '20px',
-                            fontSize: '18px',
-                            backgroundColor: '#f9f9f9',
-                            boxShadow: '0 2px 4px rgba(0, 0, 0, 0.05)',
-                            color: '#555'
-                        }}>
-                            <div>
-                                <strong>Dining hall:</strong> {order.orderDetails.dining_hall}<br />
-                                <strong>Order:</strong> {order.orderDetails.food_order}<br />
-                                <strong>Status:</strong> Completed <br />
-                                <span onClick={() => toggleFavorite(order.orderId)} style={{ cursor: 'pointer' }}>
-                                    {order.orderDetails.favorite ? '🌟' : '☆'}
-                                </span>
-                            </div>
-                        </li>
-                    ))}
-                </ul>
-            ) : (
-                <p style={{
+            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+                <h2>Previous orders:</h2>
+                <button
+                    style={{
+                        backgroundColor: '#747bff',
+                        color: '#fff',
+                        border: 'none',
+                        borderRadius: '10px',
+                        padding: '10px 20px',
+                        cursor: 'pointer',
+                        fontSize: '18px',
+                        marginBottom: '10px'
+                    }}
+                    onClick={() => setDropdownOpen(!dropdownOpen)}
+                >
+                    {dropdownOpen ? 'Close ⬆️' : 'Sort by ⬇️'}
+                </button>
+            </div>
+            {dropdownOpen && (
+                <div style={{
+                    backgroundColor: 'white',
                     border: '1px solid #ccc',
                     borderRadius: '10px',
-                    marginBottom: '10px',
-                    padding: '20px',
-                    fontSize: '18px',
-                    backgroundColor: '#f9f9f9',
-                    boxShadow: '0 2px 4px rgba(0, 0, 0, 0.05)',
-                    color: '#555'
-                }} >No previous orders found :\</p>
+                    position: 'absolute',
+                    padding: '10px',
+                    boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)',
+                    width: '150px',
+                    zIndex: 1 // Ensure dropdown stays above other elements
+                }}>
+                    <div
+                        style={{
+                            padding: '10px',
+                            cursor: 'pointer',
+                            backgroundColor: sortBy === "favorite" ? '#747bff' : 'transparent',
+                            color: sortBy === "favorite" ? '#fff' : 'black'
+                        }}
+                        onClick={() => handleSortChange("favorite")}
+                        title="Click to toggle"
+                    >
+                        Favorite 🌟 {sortBy === "favorite" ? "✅" : "🚫"}
+                    </div>
+                    <div
+                        style={{
+                            padding: '10px',
+                            cursor: 'pointer',
+                            backgroundColor: sortBy === "order" ? '#747bff' : 'transparent',
+                            color: sortBy === "order" ? '#fff' : 'black'
+                        }}
+                        onClick={() => handleSortChange("order")}
+                        title="Click to toggle"
+                    >
+                        Order (A-Z) 🍳 {sortBy === "order" ? "✅" : "🚫"}
+                    </div>
+                    <div
+                        style={{
+                            padding: '10px',
+                            cursor: 'pointer',
+                            backgroundColor: sortBy === "location" ? '#747bff' : 'transparent',
+                            color: sortBy === "location" ? '#fff' : 'black',
+                            whiteSpace: 'nowrap' // Ensure it stays on one line
+                        }}
+                        onClick={() => handleSortChange("location")}
+                        title="Click to toggle"
+                    >
+                        Location (A-Z) 📍 {sortBy === "location" ? "✅" : "🚫"}
+                    </div>
+                </div>
             )}
+
+            <ul style={{ listStyleType: 'none', padding: 0 }}>
+                {sortedPastOrders.map(order => (
+                    <li key={order.orderId} style={{
+                        border: '1px solid #ccc',
+                        borderRadius: '10px',
+                        marginBottom: '10px',
+                        padding: '20px',
+                        fontSize: '18px',
+                        backgroundColor: '#f9f9f9',
+                        boxShadow: '0 2px 4px rgba(0, 0, 0, 0.05)',
+                        color: '#555',
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'space-between'
+                    }}>
+                        <span
+                            style={{ cursor: 'pointer', fontSize: '24px', marginRight: '10px', color: order.orderDetails.favorite ? 'gold' : 'black' }}
+                            onClick={() => toggleFavorite(order.orderId)}
+                            title="Click to toggle"
+                        >
+                            {order.orderDetails.favorite ? '🌟' : '☆'}
+                        </span>
+                        <div>
+                            <strong>Dining hall:</strong> {order.orderDetails.dining_hall || ''}<br />
+                            <strong>Order:</strong> {order.orderDetails.food_order}<br />
+                            <strong>Status:</strong> {order.orderDetails.completed ? 'Completed' : 'Active'}<br />
+                        </div>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
+
